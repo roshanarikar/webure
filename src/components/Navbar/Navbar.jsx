01cbeec1aece2e8dbfaeb8333fde306db4c9e6b9
Roshan Arikar <85448130+roshanarikar@users.noreply.github.com>
2023-02-03 10:04:39 +0530

    navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,18 @@
-
+import  "./Navbar.css"
 
 export const Navbar = () =>{
     return(
-        <div>Navbar</div>
+        <div id="NavDiv">
+            <div id="logo"><p>Webure Technologies</p></div>
+            <div id="SmallNavDiv">
+                <div><p>About</p></div>
+                <div><p>Services</p></div>
+                <div><p>Careers</p></div>
+                <div><p>ROI Stories</p></div>
+                <div><p>Blog</p></div>
+                <div><p>Contact Us</p></div>
+                <div><button id="talkBtn">Let's talk</button></div>
+            </div>
+        </div>
     )
 }
